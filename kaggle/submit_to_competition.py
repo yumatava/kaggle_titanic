91662992b0d63649c01dd6ee2c72ef3009cb87ed
Yuma Taniguchi <yuma.taniguchi@avanade.com>
2023-08-27 15:35:40 +0900

    modify submit_to_competition.py

--- kaggle/submit_to_competition.py
+++ kaggle/submit_to_competition.py
@@ -1,12 +1,14 @@
 import pathlib
 import json
 
+from kaggle.api.kaggle_api_extended import KaggleApi
 
-submission_file_path = './data/output/sub.csv'
-message = 'Auto-submission'
-competition_id = 'first-competition-titanic'
 
-# jsonファイルの配置
+SUBMISSION_FILE_PATH = './data/output/sub.csv'
+MESSAGE = 'Auto-submission'
+COMPETITION_ID = 'first-competition-titanic'
+
+# Kaggle API credentials
 creds = json.load(open('./configs/kaggle.json'))
 cred_path = pathlib.Path('~/.kaggle/kaggle.json').expanduser()
 if not cred_path.exists():
@@ -14,9 +16,7 @@ if not cred_path.exists():
     cred_path.write_text(json.dumps(creds))
     cred_path.chmod(0o600)
 
-from kaggle.api.kaggle_api_extended import KaggleApi
-
 # Kaggle API
 api = KaggleApi()
 api.authenticate()
-api.competition_submit(submission_file_path, message, competition_id)
+api.competition_submit(SUBMISSION_FILE_PATH, MESSAGE, COMPETITION_ID)
